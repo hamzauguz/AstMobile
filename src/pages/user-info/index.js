52f69e7b91ad8c34505a1184ac46c99519550696
HamzaDev <arastiranhamza@gmail.com>
2024-04-02 21:46:28 +0300

    added react-native-step-indicator and integrated in userInfo

--- src/pages/user-info/index.js
+++ src/pages/user-info/index.js
@@ -21,17 +21,44 @@ import moment from 'moment';
 import CityModal from '../../components/city-modal';
 import LinearGradient from 'react-native-linear-gradient';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-
+import StepIndicator from 'react-native-step-indicator';
 import {addDoc, collection, db} from '../../utils/firebase';
 import {useNavigation} from '@react-navigation/native';
 import {useSelector} from 'react-redux';
 import {
   formatWithoutSecondTime,
   getBirthdateToHoroscopeDate,
+  windowWidth,
 } from '../../utils/helpers';
 import styles from './styles';
 
 moment.locale('tr');
+
+const labels = ['Ad & Soyad', 'Tel & Şehir', 'Doğum Tarihi & Saati'];
+const customStyles = {
+  stepIndicatorSize: 25,
+  currentStepIndicatorSize: 32,
+  separatorStrokeWidth: 2,
+  currentStepStrokeWidth: 3,
+  stepStrokeCurrentColor: 'white',
+  stepStrokeWidth: 3,
+  stepStrokeFinishedColor: 'white',
+  stepStrokeUnFinishedColor: 'purple',
+  separatorFinishedColor: 'purple',
+  separatorUnFinishedColor: '#aaaaaa',
+  stepIndicatorFinishedColor: 'purple',
+  stepIndicatorUnFinishedColor: '#ffffff',
+  stepIndicatorCurrentColor: 'black',
+  stepIndicatorLabelFontSize: 13,
+  currentStepIndicatorLabelFontSize: 16,
+  stepIndicatorLabelCurrentColor: 'white',
+  stepIndicatorLabelFinishedColor: '#ffffff',
+  stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+  labelColor: 'white',
+  labelSize: 13,
+  currentStepLabelColor: 'white',
+};
+
 const UserInfo = () => {
   const [date, setDate] = useState(new Date());
   const [open, setOpen] = useState(false);
@@ -39,7 +66,7 @@ const UserInfo = () => {
   const [birthTime, setBirthTime] = useState(new Date());
   const [openBirthTimePicker, setOpenBirthTimePicker] = useState(false);
   const [progressBar, setProgressBar] = useState(false);
-
+  const [currentPosition, setCurrentPosition] = useState(0);
   const [isModalVisible, setModalVisible] = useState(false);
   const {user} = useSelector(state => state.user);
   const [userForm, setUserForm] = useState({
@@ -98,6 +125,26 @@ const UserInfo = () => {
     }
   };
 
+  const backStepPosition = () => {
+    if (currentPosition >= 1) {
+      setCurrentPosition(prev => prev - 1);
+    }
+  };
+  const nextStepPosition = () => {
+    if (currentPosition === 0 && userForm.fullName === '') {
+      Alert.alert('Uyarı', 'Lütfen isminizi giriniz.');
+    } else if (
+      currentPosition === 1 &&
+      (userForm.phone === '' ||
+        userForm.phone.length !== 10 ||
+        userForm.city === '')
+    ) {
+      Alert.alert('Uyarı', 'Lütfen Tel & Şehir bilgilerinizi eksiksiz giriniz');
+    } else {
+      setCurrentPosition(prev => prev + 1);
+    }
+  };
+
   return (
     <Container>
       <SafeAreaView style={styles.safeAreaContainer}>
@@ -117,85 +164,150 @@ const UserInfo = () => {
           scrollEnabled
           extraScrollHeight={Platform.OS === 'ios' ? 130 : 0}
           resetScrollToCoords={{x: 0, y: 0}}
-          style={{width: '100%', flexGrow: 1}}>
-          <InputWithLabel
-            label={'Ad & Soyad'}
-            placeholder={'Adınızı ve soyadınızı giriniz.'}
-            value={userForm.fullName}
-            onChangeText={value => handleInputChange('fullName', value)}
+          style={{width: '100%', flexGrow: 1, marginTop: 30}}>
+          <StepIndicator
+            customStyles={customStyles}
+            currentPosition={currentPosition}
+            stepCount={3}
+            labels={labels}
           />
-          <View style={styles.itemwithLabel}>
-            <Text style={styles.labelStyle}>Tel No</Text>
-            <MaskInput
-              style={[styles.customButton, {fontSize: 16}]}
-              placeholderTextColor={'white'}
-              value={userForm.phone}
-              onChangeText={(masked, unmasked) =>
-                handleInputChange('phone', unmasked)
-              }
-              mask={phoneNumberRegex}
-            />
+          <View style={{marginTop: 30}}>
+            {currentPosition === 0 && (
+              <>
+                <InputWithLabel
+                  label={'Ad & Soyad'}
+                  placeholder={'Adınızı ve soyadınızı giriniz.'}
+                  value={userForm.fullName}
+                  onChangeText={value => handleInputChange('fullName', value)}
+                />
+                <View style={styles.stepButtonContainer}>
+                  <TouchableOpacity
+                    style={styles.backButtonStyle}
+                    onPress={backStepPosition}>
+                    <Text style={{color: 'white'}}>Geri</Text>
+                  </TouchableOpacity>
+                  <LinearGradient
+                    colors={['#b717d2', '#ce25ab']}
+                    start={{x: 1, y: 0}}
+                    end={{x: 0, y: 0}}
+                    style={styles.nextButtonStyle}>
+                    <TouchableOpacity
+                      onPress={nextStepPosition}
+                      style={styles.button}>
+                      <Text style={styles.saveInfoTextStyle}>Devam</Text>
+                    </TouchableOpacity>
+                  </LinearGradient>
+                </View>
+              </>
+            )}
+            {currentPosition === 1 && (
+              <>
+                <View style={styles.itemwithLabel}>
+                  <Text style={styles.labelStyle}>Tel No</Text>
+                  <MaskInput
+                    style={[styles.customButton, {fontSize: 16, marginTop: 5}]}
+                    placeholderTextColor={'white'}
+                    value={userForm.phone}
+                    onChangeText={(masked, unmasked) =>
+                      handleInputChange('phone', unmasked)
+                    }
+                    mask={phoneNumberRegex}
+                  />
+                </View>
+                <View style={styles.itemwithLabel}>
+                  <Text style={styles.labelStyle}>Şehir</Text>
+                  <TouchableOpacity
+                    onPress={toggleModal}
+                    style={styles.customButton}>
+                    <Text style={styles.citySelectTextStyle}>
+                      {userForm.city === '' ? 'Şehir Seçiniz.' : userForm.city}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.stepButtonContainer}>
+                  <TouchableOpacity
+                    style={styles.backButtonStyle}
+                    onPress={backStepPosition}>
+                    <Text style={{color: 'white'}}>Geri</Text>
+                  </TouchableOpacity>
+                  <LinearGradient
+                    colors={['#b717d2', '#ce25ab']}
+                    start={{x: 1, y: 0}}
+                    end={{x: 0, y: 0}}
+                    style={styles.nextButtonStyle}>
+                    <TouchableOpacity
+                      onPress={nextStepPosition}
+                      style={styles.button}>
+                      <Text style={styles.saveInfoTextStyle}>Devam</Text>
+                    </TouchableOpacity>
+                  </LinearGradient>
+                </View>
+              </>
+            )}
+            {currentPosition === 2 && (
+              <>
+                <View style={styles.itemwithLabel}>
+                  <Text style={styles.labelStyle}>Doğum Tarihi</Text>
+                  <TouchableOpacity
+                    style={styles.customButton}
+                    onPress={() => setOpen(true)}>
+                    <Text style={styles.dateSelectStyle}>
+                      {date.toLocaleDateString('tr-TR')}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <DatePicker
+                  modal
+                  mode="date"
+                  open={open}
+                  date={date}
+                  androidVariant="iosClone"
+                  onConfirm={date => {
+                    setOpen(false);
+                    setDate(date);
+                  }}
+                  onCancel={() => {
+                    setOpen(false);
+                  }}
+                  locale="tr"
+                />
+                <View style={styles.itemwithLabel}>
+                  <Text style={styles.labelStyle}>Doğum Saati</Text>
+                  <TouchableOpacity
+                    style={styles.customButton}
+                    onPress={() => setOpenBirthTimePicker(true)}>
+                    <Text style={styles.dateSelectStyle}>
+                      {formatWithoutSecondTime(birthTime)}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.stepButtonContainer}>
+                  <TouchableOpacity
+                    style={styles.backButtonStyle}
+                    onPress={backStepPosition}>
+                    <Text style={{color: 'white'}}>Geri</Text>
+                  </TouchableOpacity>
+                  <LinearGradient
+                    colors={['#b717d2', '#ce25ab']}
+                    start={{x: 1, y: 0}}
+                    end={{x: 0, y: 0}}
+                    style={styles.nextButtonStyle}>
+                    <TouchableOpacity
+                      onPress={() => addUserInfo()}
+                      style={styles.button}>
+                      {progressBar ? (
+                        <ActivityIndicator size={'large'} color={'white'} />
+                      ) : (
+                        <Text style={styles.saveInfoTextStyle}>
+                          Bilgilerimi Kaydet
+                        </Text>
+                      )}
+                    </TouchableOpacity>
+                  </LinearGradient>
+                </View>
+              </>
+            )}
           </View>
-          <View style={styles.itemwithLabel}>
-            <Text style={styles.labelStyle}>Şehir</Text>
-            <TouchableOpacity onPress={toggleModal} style={styles.customButton}>
-              <Text style={styles.citySelectTextStyle}>
-                {userForm.city === '' ? 'Şehir Seçiniz.' : userForm.city}
-              </Text>
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.itemwithLabel}>
-            <Text style={styles.labelStyle}>Doğum Tarihi</Text>
-            <TouchableOpacity
-              style={styles.customButton}
-              onPress={() => setOpen(true)}>
-              <Text style={styles.dateSelectStyle}>
-                {date.toLocaleDateString('tr-TR')}
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <DatePicker
-            modal
-            mode="date"
-            open={open}
-            date={date}
-            androidVariant="iosClone"
-            onConfirm={date => {
-              setOpen(false);
-              setDate(date);
-            }}
-            onCancel={() => {
-              setOpen(false);
-            }}
-            locale="tr"
-          />
-          <View style={styles.itemwithLabel}>
-            <Text style={styles.labelStyle}>Doğum Saati</Text>
-            <TouchableOpacity
-              style={styles.customButton}
-              onPress={() => setOpenBirthTimePicker(true)}>
-              <Text style={styles.dateSelectStyle}>
-                {formatWithoutSecondTime(birthTime)}
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <LinearGradient
-            colors={['#b717d2', '#ce25ab']}
-            start={{x: 1, y: 0}}
-            end={{x: 0, y: 0}}
-            style={styles.linearGradientSaveInfo}>
-            <TouchableOpacity
-              onPress={() => addUserInfo()}
-              style={styles.button}>
-              {progressBar ? (
-                <ActivityIndicator size={'large'} color={'white'} />
-              ) : (
-                <Text style={styles.saveInfoTextStyle}>Bilgilerimi Kaydet</Text>
-              )}
-            </TouchableOpacity>
-          </LinearGradient>
-
           <DatePicker
             modal
             mode="time"
--- src/pages/user-info/styles.js
+++ src/pages/user-info/styles.js
@@ -12,6 +12,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingLeft: 10,
     color: 'white',
+    marginTop: 5,
   },
   customHeaderStyle: {
     marginTop: Platform.OS === 'ios' ? 0 : 50,
@@ -25,7 +26,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   itemwithLabel: {width: '100%', alignItems: 'center', marginTop: 10},
-  labelStyle: {color: 'white', fontSize: 18, width: '90%'},
+  labelStyle: {
+    color: 'white',
+    fontSize: 18,
+    width: '90%',
+    fontFamily: 'EBGaramond-Bold',
+  },
   dateSelectStyle: {
     color: 'white',
     fontSize: 16,
@@ -39,6 +45,31 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   saveInfoTextStyle: {color: 'white', fontWeight: '600', fontSize: 18},
+  stepButtonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    width: '90%',
+    alignSelf: 'center',
+  },
+  backButtonStyle: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '25%',
+    height: 50,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  nextButtonStyle: {
+    borderRadius: 10,
+    width: '70%',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
 });
 
 export default styles;
